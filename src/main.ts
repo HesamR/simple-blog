15e936c6c5f48f00a1a956a42cc87c891b43720b
HesamR <hesam_razavi@hotmail.com>
2023-10-11 20:19:42 +0330

    fix frontend api end point

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({ credentials: true, origin: true });
+  app.setGlobalPrefix('api/v1');
 
   const configService = app.get(ConfigService);
 
