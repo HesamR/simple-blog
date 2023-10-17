d08109b1a96c7d231260e793b6513af962e6c109
HesamR <hesam_razavi@hotmail.com>
2023-10-18 00:42:22 +0330

    change mailer config

--- src/config/mailer.config.ts
+++ src/config/mailer.config.ts
@@ -9,16 +9,14 @@ export default registerAs(
   'mailer',
   () =>
     ({
-      transport: isDev()
-        ? { jsonTransport: true }
-        : {
-            host: process.env.MAILER_HOST,
-            port: process.env.MAILER_PORT,
-            auth: {
-              user: process.env.MAILER_USERNAME,
-              pass: process.env.MAILER_PASSWORD,
-            },
-          },
+      transport: {
+        host: process.env.MAILER_HOST,
+        port: process.env.MAILER_PORT,
+        auth: {
+          user: process.env.MAILER_USERNAME,
+          pass: process.env.MAILER_PASSWORD,
+        },
+      },
       defaults: { from: '"No Reply" <no-reply@hesamr.top>' },
       template: {
         dir: join(__dirname, '../..', 'templates'),
